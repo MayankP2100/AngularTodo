56bdbfda72efac7eb881f1a1d73a6d997ba94b42
Mayank <palmayank2100@gmail.com>
2024-09-07 16:06:00 +0530

    Init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {TodosComponent} from "./components/todos/todos.component";
+import {AddComponent} from "./components/add/add.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [TodosComponent, AddComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'AngularToDo';
+  title: string = 'CheckMate';
+  titleDescription: string = 'An Angular18 Todo App!';
 }
